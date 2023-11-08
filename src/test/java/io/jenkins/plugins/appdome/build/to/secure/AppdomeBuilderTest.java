7c531389dd886a6c49e9915ad079c933455ef6c4
Idan Hauser <idanhauser@gmail.com>
2023-11-08 16:32:15 +0200

    test timeout

--- src/test/java/io/jenkins/plugins/appdome/build/to/secure/AppdomeBuilderTest.java
+++ src/test/java/io/jenkins/plugins/appdome/build/to/secure/AppdomeBuilderTest.java
@@ -2,6 +2,7 @@ package io.jenkins.plugins.appdome.build.to.secure;
 
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.Timeout;
 import org.jvnet.hudson.test.JenkinsRule;
 
 import java.util.concurrent.TimeUnit;
@@ -11,11 +12,21 @@ public class AppdomeBuilderTest {
 
     @Rule
     public JenkinsRule jenkins = new JenkinsRule();
+    @Rule
+    public Timeout globalTimeout = new Timeout(600000);
 
     @Test
     public void testConfigRoundtrip() throws InterruptedException {
-        System.out.println("This is a test message.");
+        int i = 0;
+        while (i < 100)
+        {
+            System.out.println("TEST TEST TEST TEST TEST");
+            System.out.println("TEST TEST TEST TEST TEST");
+            System.out.println("TEST TEST TEST TEST TEST");
+            i++;
+        }
         TimeUnit.MINUTES.sleep(5);
+
         assertTrue(true);
 
 
